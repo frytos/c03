dda1983d234a812b37f40882b864bcefff6e7d0c
Frytos <frytos@protonmail.com>
2024-02-19 15:31:48 +0100

    adds ex01

--- ex00/ft_strcmp.c
+++ ex00/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: argrouss <argrouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 21:20:03 by argrouss          #+#    #+#             */
-/*   Updated: 2024/02/15 21:20:08 by argrouss         ###   ########.fr       */
+/*   Updated: 2024/02/19 15:20:49 by argrouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,8 @@ int	ft_strcmp(char *s1, char *s2)
 	i = 0;
 	while (s1[i] == s2[i] && (s1[i] != 0 || s2[i] != 0))
 	{
-		printf("i = %d\ts1[i] = %c\ts2[i] = %c\n", i, s1[i], s2[i]);
 		i++;
 	}
-	printf("i = %d\ts1[i] = %c\ts2[i] = %c\n", i, s1[i], s2[i]);
 	return (s1[i] - s2[i]);
 }
 
---
+++ ex01/ft_strncmp.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: argrouss <argrouss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/19 15:21:36 by argrouss          #+#    #+#             */
+/*   Updated: 2024/02/19 15:21:41 by argrouss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n);
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	int	i;
+
+	i = 0;
+	while ((i < n && s1[i] == s2[i]) && (s1[i] != 0 || s2[i] != 0))
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+// int	main(void)
+// {
+// 	char	s1[] = "LOL";
+// 	char	s2[] = "LO";
+// 	char	s3[] = "LOLO";
+// 	char	s4[] = "LOLA";
+// 	char	s5[] = "LOLB";
+// 	char	s6[] = "";
+// 	char	s7[] = "LOL\n";
+// 	char	s8[] = "LOL ";
+// 	char	s9[] = "LOL\t";
+
+// 	printf("ft_strncmp(s1, s2) = %d\n", ft_strncmp(s1, s2, 2));
+// 	printf("ft_strncmp(s1, s2) = %d\n", ft_strncmp(s1, s3, 2));
+// 	printf("ft_strncmp(s4, s5) = %d\n", ft_strncmp(s4, s5, 2));
+// 	printf("ft_strncmp(s5, s4) = %d\n", ft_strncmp(s5, s4, 2));
+// 	printf("ft_strncmp(s6, s6) = %d\n", ft_strncmp(s6, s6, 2));
+// 	printf("ft_strncmp(s6, s1) = %d\n", ft_strncmp(s6, s1, 2));
+// 	printf("ft_strncmp(s7, s1) = %d\n", ft_strncmp(s7, s1, 2));
+// 	printf("ft_strncmp(s8, s1) = %d\n", ft_strncmp(s8, s1, 2));
+// 	printf("ft_strncmp(s9, s1) = %d\n", ft_strncmp(s9, s1, 2));
+// 	printf("ft_strncmp(s7, s8) = %d\n", ft_strncmp(s7, s8, 2));
+// 	printf("ft_strncmp(s7, s9) = %d\n", ft_strncmp(s7, s9, 2));
+// 	printf("ft_strncmp(s8, s9) = %d\n", ft_strncmp(s8, s9, 2));
+// 	return (0);
+// }
+
+int main(int argc, char **argv)
+{
+    printf("strncmp:%d\n\n",strncmp(argv[1],argv[2],atoi(argv[3])));
+    printf("ft_strncmp:%d\n\n",ft_strncmp(argv[1],argv[2],atoi(argv[3])));
+}
