f8bfef7cd415978e29a53e2377b1767561644e18
Arnaud Groussard <frytos@protonmail.com>
2024-02-20 17:21:42 +0100

    compacts code

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -23,9 +23,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 
 	i = 0;
 	while ((i < n && s1[i] == s2[i]) && (s1[i] != 0 || s2[i] != 0))
-	{
 		i++;
-	}
 	return (s1[i] - s2[i]);
 }
 
--- ex04/ft_strstr.c
+++ ex04/ft_strstr.c
@@ -28,9 +28,7 @@ char	*ft_strstr(char *str, char *to_find)
 		{
 			j = 0;
 			while (str[i + j] == to_find[j] && to_find[j] != 0)
-			{
 				j++;
-			}
 			if (to_find[j] == 0)
 				return (&str[i]);
 		}
