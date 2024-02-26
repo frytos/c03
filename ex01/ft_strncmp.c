90061c23325fa1ab9a316432f12b53959b932b3f
Frytos <86711501+frytos@users.noreply.github.com>
2024-02-26 13:44:19 +0100

    Update ft_strncmp.c

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -22,9 +22,9 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	unsigned int	i;
 
 	i = 0;
-	while ((i < n && s1[i] == s2[i]) && (s1[i] != 0 || s2[i] != 0))
+	while ((i < n - 1 && s1[i] == s2[i]) && (s1[i] != 0 || s2[i] != 0))
 		i++;
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
 // int	main(void)
